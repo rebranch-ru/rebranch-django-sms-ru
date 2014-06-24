cf708451685033228b0e94dbc0aacd8c03403e71
Сергей Дубинин <whackojacko@Mac-mini-Sergej.local>
2014-06-24 17:58:06 +0600

    sms ru moved to separate app

---
+++ rebranch_django_sms_ru/celery.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+from django.conf import settings
+
+from celery import Celery
+
+
+app = Celery(backend=settings.CELERY_RESULT_BACKEND, broker=settings.BROKER_URL)
--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ from setuptools import setup
 
 
 setup(
-    name='rebranch_sms_ru',
-    version=0.2,
+    name='rebranch_django_sms_ru',
+    version=0.1,
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     author='Rebranch',
     package_data={},
     install_requires=[u'requests'],
-    url='https://Franz_ru@bitbucket.org/Franz_ru/rebranch-deployment.git'
+    dependency_links=[u'https://github.com/rebranch/rebranch-sms-ru.git#egg=rebranch_sms_ru'],
+    url='https://github.com/rebranch/rebranch-django-sms-ru.git'
 )
