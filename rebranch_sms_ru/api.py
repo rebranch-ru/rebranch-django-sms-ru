f52530b16ec8af9aa4311607e5123d2d5e59d963
Сергей Дубинин <whackojacko@Mac-mini-Sergej.local>
2014-06-24 16:00:49 +0600

    get_status method

--- rebranch_sms_ru/api.py
+++ rebranch_sms_ru/api.py
@@ -7,12 +7,14 @@ from rebranch_sms_ru.statuses import STATUS_CHOICES
 
 
 class SMSSender(object):
+    _PARAM_MESSAGE_ID = u'id'
     _PARAM_API_ID = u'api_id'
     _PARAM_RECIPIENT = u'to'
     _PARAM_CONTENT = u'text'
 
     _API_METHOD_SEND = u'/sms/send'
     _API_METHOD_COST = u'/sms/cost'
+    _API_METHOD_STATUS = u'/sms/status'
 
     _content = None
     _api_id = None
@@ -44,6 +46,17 @@ class SMSSender(object):
     def get_status_description(status):
         return STATUS_CHOICES.get(status, STATUS_CHOICES[u'default'])
 
+    def get_status(self, sms_id):
+        params = {
+            self._PARAM_MESSAGE_ID: sms_id
+        }
+        api_response = self._call_api_method(self._API_METHOD_STATUS, **params)
+        status = api_response.text.strip()
+        result = {
+            u'status': status,
+        }
+        return result
+
     def get_cost(self, recipient, content):
         params = {
             self._PARAM_RECIPIENT: recipient,
@@ -65,6 +78,9 @@ class SMSSender(object):
         }
         api_response = self._call_api_method(self._API_METHOD_SEND, **params)
         status, sms_id, balance = (api_response.text.split() + [None, None])[:3]
+        if not status == u'100':
+            # в случае неудачи во второй строке идет номер телефона
+            sms_id = None
         result = {
             u'balance': balance,
             u'sms_id': sms_id,
