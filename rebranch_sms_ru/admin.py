d9aa64577f8e4f8de7f5fbbea15ffe46577e9810
Сергей Дубинин <whackojacko@Mac-mini-Sergej.local>
2014-06-24 11:53:59 +0600

    show related object on admin list page

--- rebranch_sms_ru/admin.py
+++ rebranch_sms_ru/admin.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from django.conf import settings
 
+from rebranch_sms_ru.utils import ModelAdminWithFKLink
 from rebranch_sms_ru.models import Message
 from rebranch_sms_ru.statuses import STATUS_CHOICES
 
@@ -21,19 +22,26 @@ class RawStatusSimpleListFilter(admin.SimpleListFilter):
             return queryset
 
 
-class MessageModelAdmin(admin.ModelAdmin):
-    list_display = (u'recipient', u'api_id', u'sent', u'queue_type', u'status_raw', u'status')
+class MessageModelAdmin(ModelAdminWithFKLink):
+    search_fields = (u'recipient', u'api_id')
+    list_filter = (u'queue_type', RawStatusSimpleListFilter)
+
+    list_display = (
+    u'recipient', u'api_id', u'sent', u'queue_type', u'status_raw', u'status', u'link_to_content_object')
     if getattr(settings, u'SMS_RU_STORE_SMS_COST', False):
         list_display += (u'cost',)
 
     def status_raw(self, obj):
         return obj.status
 
-    search_fields = (u'recipient', u'api_id')
-    list_filter = (u'queue_type', RawStatusSimpleListFilter)
-
     status_raw.allow_tags = True
     status_raw.short_description = u'Код статуса'
+
+    def link_to_content_object(self, instance):
+        return self.__getattr__('link_to_content_object')(instance)
+
+    link_to_content_object.allow_tags = True
+    link_to_content_object.short_description = u'Связанный объект'
 
 
 admin.site.register(Message, MessageModelAdmin)
