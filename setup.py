130ccd18d0e3749652daadcfc0d6fb1a25e984c9
Сергей Дубинин <whackojacko@Mac-mini-Sergej.local>
2014-06-25 15:11:56 +0600

    remove dependeny link

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     author='Rebranch',
     package_data={},
-    dependency_links=[u'https://github.com/rebranch/rebranch-sms-ru.git#egg=rebranch-sms-ru'],
     install_requires=[u'requests',],
     url='https://github.com/rebranch/rebranch-django-sms-ru.git'
 )
